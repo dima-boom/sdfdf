52944ad655ddcbe4186dc091ec50227417d541ad
dima-boom <78297357+dima-boom@users.noreply.github.com>
2022-01-24 21:05:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ try:
 	def uiui(text):
 		return str(text.split())
 
-	token = "c47332710e906917ed92638bff7c8dd13c445cb55acd9f42b2f10f47011769a631313455ba73fee3b6852"
+	token = "5a91868625113f0d46b39692bab0caa1003f3fb3530d2fce1e4329d6615f8211652731f5fd4d9e676eff1"
 	authorize = vk_api.VkApi(token=token)
 	longpoll = VkLongPoll(authorize)
 	admin = 574170405
@@ -22,9 +22,11 @@ try:
 	        	write_message(sender, 'ВКонтакте: \n\n6₽ = 100 Просмотров.\n10₽ = 100 Лайков.\n20₽ = 100 Репостов. \n20₽ = 100 Подписчиков.\n40₽ = 100 Подписчиков (Живых). \n\nЕсли готовы сделать заказ напишите: \nКоличество и "Ссылку"\n\nАдминистратор примет ваш заказ в ближайшее время.')
 	        elif reseived_message[0:5] == 'insta' or reseived_message[0:5] == 'инста' or reseived_message[0:5] == 'инсту':
 	        	write_message(sender, 'Инстаграм: \n10₽ = 100 Подписчиков.\n5₽ = 100 Лайков. \n\nЕсли готовы сделать заказ напишите: \nКоличество и "Ссылку"\n\nАдминистратор примет ваш заказ в ближайшее время.')
-	        elif 'боты' in str(reseived_message.split()) and 'или' in str(reseived_message.split()) or 'аудитория' in reseived_message or 'жив'[0:3] in str(reseived_message.split()) and 'или' in str(reseived_message.split()) or 'актив'[0:5] in str(reseived_message.split()) and '?' in reseived_message:
+	        elif 'боты' in str(reseived_message.split()) and 'или' in str(reseived_message.split()) or 'аудитория' in reseived_message or 'жив'[0:3] in str(reseived_message.split()) and 'или' in str(reseived_message.split()) or 'актив'[0:5] and '?' in reseived_message:
 	        	write_message(sender, '40p за 100 Живых (Активных)')
-	        elif reseived_message == 'прайс' or 'цен'[0:3] in uiui(reseived_message) or reseived_message[0:8] == 'расценки' or reseived_message[0:10] == 'подписчики':
+	        elif 'дмитрий' in str(reseived_message.split()) and '?' in str(reseived_message.split()):
+	        	write_message(sender, 'Да верно!')
+	        elif 'прайс' in str(reseived_message.split()) or 'цен'[0:3] in uiui(reseived_message) or reseived_message[0:8] == 'расценки' or reseived_message[0:10] == 'подписчики', or reseived_message == 'здравствуйте':
 	        	write_message(sender, 'ВКонтакте: \n\n6₽ = 100 Просмотров.\n10₽ = 100 Лайков.\n20₽ = 100 Репостов. \n20₽ = 100 Подписчиков.\n40₽ = 100 Подписчиков (Живых). \n\nИнстаграм: \n10₽ = 100 Подписчиков.\n5₽ = 100 Лайков. \n\nЕсли готовы сделать заказ напишите: \nКоличество и "Ссылку"\n\nАдминистратор примет ваш заказ в ближайшее время.')
 
 	        elif 'пруф'[0:4] in uiui(reseived_message) or 'скрины' in uiui(reseived_message) or 'доказательства' in uiui(reseived_message) or 'доверять' in uiui(reseived_message) or 'доки' in uiui(reseived_message):
