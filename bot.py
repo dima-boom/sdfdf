04abce49895bf8905a9673c18ae2dc49d72dc52c
dima-boom <78297357+dima-boom@users.noreply.github.com>
2022-02-17 19:26:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ try:
 	        	write_message(sender, 'Инстаграм: \n10₽ = 100 Подписчиков.\n5₽ = 100 Лайков. \n\nЕсли готовы сделать заказ напишите: \nКоличество и "Ссылку"\n\nАдминистратор примет ваш заказ в ближайшее время.')
 	        elif 'боты' in str(reseived_message.split()) and 'или' in str(reseived_message.split()) or 'аудитория' in reseived_message or 'жив'[0:3] in str(reseived_message.split()) and 'или' in str(reseived_message.split()) or 'актив'[0:5] and '?' in reseived_message:
 	        	write_message(sender, '40p за 100 Живых (Активных)')
-	        elif 'прайс' in str(reseived_message.split()) or 'цен'[0:3] in uiui(reseived_message) or reseived_message[0:8] == 'расценки' or reseived_message[0:10] == 'подписчики', or reseived_message == 'здравствуйте':
+	        elif 'прайс' in str(reseived_message.split()) or 'цен'[0:3] in uiui(reseived_message) or reseived_message[0:8] == 'расценки' or reseived_message[0:10] == 'подписчики' or reseived_message == 'здравствуйте':
 	        	write_message(sender, 'ВКонтакте: \n\n6₽ = 100 Просмотров.\n10₽ = 100 Лайков.\n20₽ = 100 Репостов. \n20₽ = 100 Подписчиков.\n40₽ = 100 Подписчиков (Живых). \n\nИнстаграм: \n10₽ = 100 Подписчиков.\n5₽ = 100 Лайков. \n\nЕсли готовы сделать заказ напишите: \nКоличество и "Ссылку"\n\nАдминистратор примет ваш заказ в ближайшее время.')
 	        elif 'дмитрий'[0:7] in uiui(reseived_message) and '?' in reseived_message or reseived_message[0:18] == 'дмитрий витальевич':
 	        	write_message(sender, 'Да верно! \nМожете делать перовод.')
@@ -34,12 +34,11 @@ try:
 	        	write_message(sender, 'Всегда пожалуйста ;)')
 	        elif 'qiwi'[0:4] in uiui(reseived_message) or 'киви'[0:4] in uiui(reseived_message):
 	        	user = authorize.method("users.get", {"user_ids": event.user_id})  
-            	        name = str(user[0]['first_name']) + ' ' + str(user[0]['last_name'])
-	        	write_message(sender, f'Qiwi - 79283692011 \n\nКоментарий к платежу: {name}.')
+	        	name = str(user[0]['first_name']) + ' ' + str(user[0]['last_name'])
+	        	write_message(sender, f'Qiwi - 79283692011 \n\nКоментарий к платежу: \n{name}.')
 	        elif 'карт'[0:4] in uiui(reseived_message) or reseived_message[0:4] == 'сбер' or reseived_message[0:3] == 'сбп':
-                        user = authorize.method("users.get", {"user_ids": event.user_id})  
-            	        name = str(user[0]['first_name']) + ' ' + str(user[0]['last_name'])
-	        	
-	        	write_message(sender, f'Номер карты - 4276600059773339 \nСбп - 79064601130 \nЭто Сбербанк.\n\nКоментарий к платежу: {name}.')
+	        	user = authorize.method("users.get", {"user_ids": event.user_id})  
+	        	name = str(user[0]['first_name']) + ' ' + str(user[0]['last_name'])
+	        	write_message(sender, f'Номер карты - 4276600059773339 \n\nСбп - 79064601130 \nЭто Сбербанк.\n\nКоментарий к платежу: \n{name}.')
 except:
 	os.system('python bot.py')
